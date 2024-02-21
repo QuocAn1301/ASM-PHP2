218592134b64257b5a696537898562d5ea533f1a
QuocAn1301 <137316838+QuocAn1301@users.noreply.github.com>
2024-02-21 22:57:13 +0700

    Tìm kiếm

--- resources/views/home/search.blade.php
+++ resources/views/home/search.blade.php
@@ -1,27 +1,179 @@
-<!-- Trong tệp resources/views/products/search.blade.php -->
 @extends('master.main')
 
 @section('main')
-<div class="container">
-    <h2>Kết quả tìm kiếm cho từ khóa "{{ request('query') }}"</h2>
 
-    @if($products->count() > 0)
-    <div class="row">
-        @foreach($products as $product)
-        <div class="col-md-3">
-            <div class="card">
-                <img src="{{ $product->image_url }}" class="card-img-top" alt="{{ $product->name }}">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $product->name }}</h5>
-                    <p class="card-text">{{ $product->description }}</p>
-                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">Chi tiết</a>
+<div class="shop-wrapper">
+    <!-- Breadcrumb Area Start Here -->
+    <div class="breadcrumbs-area position-relative">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <div class="breadcrumb-content position-relative section-content">
+
+                        <h2>Kết quả tìm kiếm cho từ khóa "{{ request('query') }}"</h2>
+                        </br>
+
+
+
+                    </div>
+
                 </div>
             </div>
         </div>
-        @endforeach
     </div>
-    @else
-    <p>Không tìm thấy sản phẩm nào phù hợp.</p>
-    @endif
+    <!-- Breadcrumb Area End Here -->
+    <!-- Shop Main Area Start Here -->
+    <div class="shop-main-area">
+        <div class="container container-default custom-area">
+
+            <div class="row flex-row-reverse">
+
+                <div class="col-lg-9 col-12 col-custom widget-mt">
+                    <!--shop toolbar start-->
+                    <div class="shop_toolbar_wrapper">
+                        <div class="shop_toolbar_btn">
+                            <button data-role="grid_3" type="button" class="active btn-grid-3" data-bs-toggle="tooltip"
+                                title="3"><i class="fa fa-th"></i></button>
+                            <!-- <button data-role="grid_4" type="button"  class=" btn-grid-4" data-bs-toggle="tooltip" title="4"></button> -->
+                            <button data-role="grid_list" type="button" class="btn-list" data-bs-toggle="tooltip"
+                                title="List"><i class="fa fa-th-list"></i></button>
+                        </div>
+                        <div class="shop-select">
+                            <form class="d-flex flex-column w-100" action="#">
+                                <div class="form-group">
+                                    <select class="form-control nice-select w-100">
+                                        <option selected value="1">Alphabetically, A-Z</option>
+                                        <option value="2">Sort by popularity</option>
+                                        <option value="3">Sort by newness</option>
+                                        <option value="4">Sort by price: low to high</option>
+                                        <option value="5">Sort by price: high to low</option>
+                                        <option value="6">Product Name: Z</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!--shop toolbar end-->
+                    <!-- Shop Wrapper Start -->
+
+                    <div class="container">
+                        @if($products->count() == 0)
+                        <h4>Không tìm thấy sản phẩm nào phù hợp.</h4>
+                        @elseif($products->count() > 0)
+                        <div class="row">
+                            @foreach($products as $product)
+                            <div class="col-md-3">
+                                <div class="card">
+                                    <img src="{{ $product->image_url }}" class="card-img-top"
+                                        alt="{{ $product->name }}">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $product->name }}</h5>
+                                        <p class="card-text">{{ $product->description }}</p>
+                                        <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">Chi
+                                            tiết</a>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+
+                        @endif
+                    </div>
+                    <!-- Shop Wrapper End -->
+                    <!-- Bottom Toolbar Start -->
+                    <div class="row">
+                        <div class="col-sm-12 col-custom">
+                            <div class="toolbar-bottom mt-30">
+                                <nav class="pagination pagination-wrap mb-10 mb-sm-0">
+                                    <ul class="pagination">
+                                        <li class="disabled prev">
+                                            <i class="ion-ios-arrow-thin-left"></i>
+                                        </li>
+                                        <li class="active"><a>1</a></li>
+                                        <li>
+                                            <a href="#">2</a>
+                                        </li>
+                                        <li class="next">
+                                            <a href="#" title="Next >>"><i class="ion-ios-arrow-thin-right"></i></a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                                <p class="desc-content text-center text-sm-right">Showing 1 - 12 of 34 result</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Bottom Toolbar End -->
+                </div>
+                <div class="col-lg-3 col-12 col-custom">
+                    <!-- Sidebar Widget Start -->
+                    <aside class="sidebar_widget widget-mt">
+                        <div class="widget_inner">
+
+                            <div class="widget-list widget-mb-1">
+                                <h3 class="widget-title">Menu Categories</h3>
+                                <!-- Widget Menu Start -->
+                                <nav>
+                                    <ul class="mobile-menu p-0 m-0">
+                                        <li class="menu-item-has-children"><a href="#">Breads</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Skateboard (02)</a></li>
+                                                <li><a href="#">Surfboard (4)</a></li>
+                                                <li><a href="#">Accessories (3)</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children"><a href="#">Fruits</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Samsome</a></li>
+                                                <li><a href="#">GL Stylus (4)</a></li>
+                                                <li><a href="#">Uawei (3)</a></li>
+                                                <li><a href="#">Avocado (3)</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children"><a href="#">Vagetables</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Power Bank</a></li>
+                                                <li><a href="#">Data Cable (4)</a></li>
+                                                <li><a href="#">Avocado (3)</a></li>
+                                                <li><a href="#">Brocoly (3)</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children"><a href="#">Organic Food</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Vagetables</a></li>
+                                                <li><a href="#">Green Food (4)</a></li>
+                                                <li><a href="#">Coconut (3)</a></li>
+                                                <li><a href="#">Cabage (3)</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </nav>
+                                <!-- Widget Menu End -->
+                            </div>
+                            <div class="widget-list widget-mb-1">
+                                <h3 class="widget-title">Categories</h3>
+                                <div class="sidebar-body">
+                                    <ul class="sidebar-list">
+                                        <li><a href="#">All Product</a></li>
+                                        <li><a href="#">Best Seller (5)</a></li>
+                                        <li><a href="#">Featured (4)</a></li>
+                                        <li><a href="#">New Products (6)</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </aside>
+                    <!-- Sidebar Widget End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Shop Main Area End Here -->
+    <!-- Support Area Start Here -->
+    <!-- Support Area End Here -->
+
+    <!-- Footer Area End Here -->
 </div>
+
 @stop
